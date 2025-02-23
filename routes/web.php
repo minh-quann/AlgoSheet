72f46c64066dc5fa8f7a5fdc130b561c262d71c4
minh-quann <quannguyen19092003@gmail.com>
2025-02-23 22:44:57 +0700

    Firt Commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::middleware(['web'])->group(function () {
+    Route::group(['prefix' => 'admin'], function () {
+        Route::group(['middleware' => 'admin.guest'], function () {
+            Route::get('/login', [AdminLoginController::class, 'index'])->name('admin.login');
+            Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+        });
+
+        Route::group(['middleware' => 'admin.auth'], function () {
+            Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+            Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
+        });
+    });
+});
